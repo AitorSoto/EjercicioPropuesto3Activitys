123af2e08e794e73621d08ac18ad99dfd3df72b2
AitorSoto <aitor.soto@iesdoctorbalmis.com>
2019-10-18 12:03:18 +0200

    Abre intent

--- app/src/main/java/com/example/ejerciciopropuesto3activitys/MainActivity.java
+++ app/src/main/java/com/example/ejerciciopropuesto3activitys/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button btActivity1;
     private Button btActivity2;
@@ -20,11 +20,19 @@ public class MainActivity extends AppCompatActivity {
         btActivity1 = (Button) findViewById(R.id.btActivity1);
         btActivity2 = (Button) findViewById(R.id.btActivity2);
 
-        btActivity1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Activity1.class);
-            }
-        });
+        btActivity1.setOnClickListener(this); // Da de alta la clase en el Manifest
+    }
+
+    public void onClick(View view){
+        if (view.getId() == R.id.btActivity1) {
+            Intent intent = new Intent(this, Activity1.class);
+            intent.putExtra("Entrada", "Activity 1");
+            startActivity(intent);
+        } else{
+            Intent intent = new Intent(this, Activity1.class);
+            intent.putExtra("Entrada", "Activity 1");
+            startActivity(intent);
+        }
+
     }
 }
